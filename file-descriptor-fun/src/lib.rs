29e4cd595146f7cdb172161f13c7597afeb4b546
Andreas Fuchs <asf@boinkor.net>
2016-04-20 16:06:50 -0700

    Add a super simple round-trip test
    It works!!! I can actually send bytes across.

--- file-descriptor-fun/src/lib.rs
+++ file-descriptor-fun/src/lib.rs
@@ -31,17 +31,44 @@ mod filedes {
         try!(socket::listen(socket, MAX_BACKLOG_QUEUE));
         Ok(socket)
     }
+
+    pub fn connect_to_socket(path: &str) -> Result<RawFd, nix::Error> {
+        let socket = try!(socket::socket(socket::AddressFamily::Unix, socket::SockType::Stream, socket::SockFlag::empty(), 0));
+        let sockaddr = try!(make_socket_addr(path));
+        try!(socket::connect(socket, &sockaddr));
+        Ok(socket)
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use filedes;
+    use std::str;
+    use std::thread;
+    use nix::sys::socket;
+    use nix::unistd;
 
     #[test]
-    fn it_works() {
+    fn it_does_very_simple_things() {
         filedes::setup().unwrap();
         filedes::make_socket_addr("mysock").unwrap();
         filedes::server_socket("mysock").unwrap();
+    }
 
+    #[test]
+    fn it_roundtrips() {
+        filedes::setup().unwrap();
+        let s_sock = filedes::server_socket("mysock2").unwrap();
+        thread::spawn(move || {
+            let conn = socket::accept(s_sock).unwrap();
+            socket::send(conn, "flub".as_bytes(), socket::MsgFlags::empty()).unwrap();
+            unistd::close(s_sock).unwrap();
+        });
+        let sock = filedes::connect_to_socket("mysock2").unwrap();
+
+        let mut buf: [u8; 5] = [0, 0, 0, 0, 0];
+        let received_bytes = socket::recv(sock, &mut buf, socket::MsgFlags::empty()).unwrap();
+        assert_eq!(received_bytes, 4);
+        assert_eq!(str::from_utf8(&buf[0..received_bytes]).unwrap(), "flub");
     }
 }
