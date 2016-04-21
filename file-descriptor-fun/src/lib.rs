483fd0730411694298d73fe60a4af84cb6dda16d
Andreas Fuchs <asf@boinkor.net>
2016-04-20 17:15:57 -0700

    Add a simple test for sending FDs

--- file-descriptor-fun/src/lib.rs
+++ file-descriptor-fun/src/lib.rs
@@ -46,6 +46,8 @@ mod tests {
     use std::str;
     use std::thread;
     use nix::sys::socket;
+    use nix::sys::uio::IoVec;
+    use std::os::unix::io::RawFd;
     use nix::unistd;
 
     #[test]
@@ -56,7 +58,7 @@ mod tests {
     }
 
     #[test]
-    fn it_roundtrips() {
+    fn it_sends_data() {
         filedes::setup().unwrap();
         let s_sock = filedes::server_socket("mysock2").unwrap();
         thread::spawn(move || {
@@ -70,5 +72,29 @@ mod tests {
         let received_bytes = socket::recv(sock, &mut buf, socket::MsgFlags::empty()).unwrap();
         assert_eq!(received_bytes, 4);
         assert_eq!(str::from_utf8(&buf[0..received_bytes]).unwrap(), "flub");
+    }
+
+    #[test]
+    fn it_sends_fds() {
+        filedes::setup().unwrap();
+        let s_sock = filedes::server_socket("mysock3").unwrap();
+        thread::spawn(move || {
+            let conn = socket::accept(s_sock).unwrap();
+
+            let buf = vec!(IoVec::from_slice("!".as_bytes()));
+
+            let fds = vec!(conn);
+            let cmsgs = vec!(socket::ControlMessage::ScmRights(fds.as_slice()));
+            socket::sendmsg(conn, &buf.as_slice(), cmsgs.as_slice(), socket::MsgFlags::empty(), None).unwrap();
+            unistd::close(s_sock).unwrap();
+        });
+        let sock = filedes::connect_to_socket("mysock3").unwrap();
+
+        let mut backing_buf = vec!(0);
+        let mut buf = vec!(IoVec::from_mut_slice(&mut backing_buf));
+        let mut cmsg: socket::CmsgSpace<([RawFd; 15])> = socket::CmsgSpace::new();
+        let msg = socket::recvmsg(sock, &mut buf.as_mut_slice(), Some(&mut cmsg), socket::MsgFlags::empty()).unwrap();
+        assert_eq!(1, msg.cmsgs().count());
+
     }
 }
