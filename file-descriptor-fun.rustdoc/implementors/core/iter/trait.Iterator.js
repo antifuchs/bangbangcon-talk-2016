e9abbe02355a9e63b1bb1aaa1e3f3d2af0317743
Andreas Fuchs <asf@boinkor.net>
2016-05-11 23:02:57 -0700

    Add rustdoc

---
+++ file-descriptor-fun.rustdoc/implementors/core/iter/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['libc'] = [];implementors['nix'] = ["impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='nix/sys/socket/struct.CmsgIterator.html' title='nix::sys::socket::CmsgIterator'>CmsgIterator</a>&lt;'a&gt;",];implementors['filedes'] = ["impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='filedes/ring/struct.RingIter.html' title='filedes::ring::RingIter'>RingIter</a>&lt;'a&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
