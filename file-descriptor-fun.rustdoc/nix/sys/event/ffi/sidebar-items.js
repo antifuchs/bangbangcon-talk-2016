5595f9baad0de4b0298316b6c26421439c4e32b0
Andreas Fuchs <asf@boinkor.net>
2016-05-15 12:18:00 -0700

    Update rustdoc some more

--- file-descriptor-fun.rustdoc/nix/sys/event/ffi/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"enum":[["c_void",""]],"fn":[["kevent",""]],"struct":[["kevent",""],["timespec",""]],"type":[["c_int",""],["int64_t",""],["intptr_t",""],["size_t",""],["uintptr_t",""]]});
